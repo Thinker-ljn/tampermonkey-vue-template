716245e4cc13bf7bf4cc9622b04776069836a211
Thinker-ljn <thinker_ljn@163.com>
2019-04-12 13:06:35 +0800

    remove container id

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import App from './app.vue'
 import Vue from 'vue'
 
 const appContainer = document.createElement('div')
-appContainer.id = 'tampermonkey-replace-container'
 document.body.appendChild(appContainer)
 
 const vm = new Vue({
